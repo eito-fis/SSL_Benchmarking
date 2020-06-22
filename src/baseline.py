4e6bc20b03e2e591e0e57d6396f367c55c0ea419
Eamon Ito-Fisher <eamon@itofisher.com>
2020-06-22 13:28:01 -0700

    Added arguments and support for training VAT

--- src/baseline.py
+++ src/baseline.py
@@ -1,33 +1,67 @@
 import pickle
+import argparse
 from sklearn.model_selection import train_test_split
 
 from finetune import SequenceLabeler
+from finetune.target_models.semi_suprevised import SSLLabeler
 from finetune.base_models import RoBERTa
 from finetune.util.metrics import annotation_report
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Train SSL')
+    parser.add_argument('--algo',
+                        type=str,
+                        default=None,
+                        help="""What algorithm to train. Current options are
+                        (VAT).  Unrecognized strs or None trains a default CRF
+                        RoBERTa Sequence Labeler. (default: None)""")
+    parser.add_argument('--data-usage',
+                        type=int,
+                        default=100,
+                        help="""What percent of the labeled data that will be
+                        used as labeled data, should be an int 1 - 100.
+                        (default: 100)""")
+    args = parser.parse_args()
+
+
     filename = "data/CONLL-2003/processed.pickle"
     with open(filename, mode="rb") as f:
         dataset = pickle.load(f)
     allX = dataset["inputs"]
     allY = dataset["labels"]
-    allX = allX[:2500]
-    allY = allY[:2500]
+    al = len(allX)
     trainX, testX, trainY, testY = train_test_split(
         allX,
         allY,
-        test_size=0.7,
+        test_size=0.2,
         random_state=40
     )
-    model = SequenceLabeler(base_model=RoBERTa,
-                            batch_size=1, n_epochs=3,
-                            val_size=0.0, max_length=16,
-                            chunk_long_sequences=True,
-                            subtoken_predictions=True,
-                            crf_sequence_labeling=True)
-    model.fit(trainX, trainY)
+    split = 1 - args.data_usage / 100
+    if split > 0.0:
+        trainX, unlabeledX, trainY, _ = train_test_split(
+            trainX,
+            trainY,
+            test_size= 1 - args.data_usage / 100,
+            random_state=40
+        )
+    else:
+        unlabeledX = []
+    l = len(trainX)
+    p = (l / al) * 100
+    print(f"{l} examples labeled of {al} available - {p:.2f}% of the data")
+    l = len(unlabeledX)
+    p = (l / al) * 100
+    print(f"{l} examples unlabeled of {al} available - {p:.2f}% of the data")
+
+    if args.algo == "VAT":
+        model = SSLLabeler(base_model=RoBERTa,
+                           crf_sequence_labeling=False)
+        model.fit(trainX, Us=unlabeledX, Y=trainY)
+    else:
+        model = SequenceLabeler(base_model=RoBERTa)
+        model.fit(trainX, trainY)
+
     predictions = model.predict(testX)
-    print(predictions)
     print(annotation_report(testY,
                             predictions))
