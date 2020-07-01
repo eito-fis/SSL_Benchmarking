628ea06294e0c4c14853907cf43e0e4011370151
Eamon Ito-Fisher <eamon@itofisher.com>
2020-07-01 09:12:25 -0700

    Updated arguments and argument handling

--- src/baseline.py
+++ src/baseline.py
@@ -11,12 +11,23 @@ from finetune.util.metrics import annotation_report
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Train SSL')
+    parser.add_argument('--data',
+                        type=str,
+                        default="data/CONLL-2003/processed.pickle",
+                        help="""Pickle file to load data from. (default:
+                        data/CONLL-2003/processed.pickle""")
     parser.add_argument('--algo',
                         type=str,
                         default=None,
                         help="""What algorithm to train. Current options are
                         (VAT, Pseudo).  Unrecognized strs or None trains a
-                        default RoBERTa Sequence Labeler. (default: None)""")
+                        Sequence Labeler. (default: None)""")
+    parser.add_argument('--base-model',
+                        type=str,
+                        default=None,
+                        help="""What basemodel to train. Current options are
+                        (RoBERTa, TCN).  Unrecognized strs or None trains a
+                        RoBERTa model. (default: None)""")
     parser.add_argument('--data-usage',
                         type=int,
                         default=100,
@@ -31,10 +42,14 @@ if __name__ == "__main__":
                         default=False,
                         action='store_true',
                         help="Whether or not to use a CRF. (default: False)")
+    parser.add_argument('--low-memory',
+                        default=False,
+                        action='store_true',
+                        help="Whether or not to use low memory mode. (default: False)")
     parser.add_argument('--wandb',
                         default=False,
                         action='store_true',
-                        help="Whether or not to use a CRF. (default: False)")
+                        help="Whether or not to use WandB logging. (default: False)")
     args = parser.parse_args()
 
     if args.wandb:
@@ -46,7 +61,7 @@ if __name__ == "__main__":
         hooks = None
 
 
-    filename = "data/CONLL-2003/processed.pickle"
+    filename = args.data
     with open(filename, mode="rb") as f:
         dataset = pickle.load(f)
     allX = dataset["inputs"]
@@ -56,7 +71,7 @@ if __name__ == "__main__":
         allX,
         allY,
         test_size=0.2,
-        random_state=40
+        random_state=42
     )
     split = 1 - args.data_usage / 100
     if split > 0.0:
@@ -64,7 +79,7 @@ if __name__ == "__main__":
             trainX,
             trainY,
             test_size= 1 - args.data_usage / 100,
-            random_state=40
+            random_state=42
         )
     else:
         unlabeledX = []
@@ -75,27 +90,40 @@ if __name__ == "__main__":
     p = (l / al) * 100
     print(f"{l} examples unlabeled of {al} available - {p:.2f}% of the data")
 
-    base_model = RoBERTa
-    if args.algo == "VAT":
+    arg_base_model = None if not args.base_model else args.base_model.lower()
+    if arg_base_model == "tcn":
+        base_model = TCN
+    else:
+        base_model = RoBERTa
+    algo = None if not args.algo else args.algo.lower()
+    if algo == "vat":
         model = VATLabeler(base_model=base_model,
                            crf_sequence_labeling=args.crf,
                            n_epochs=args.epochs,
                            tensorboard_folder="tensorboard/vat",
                            val_interval=2,
-                           val_size=0)
+                           val_size=0,
+                           low_memory_mode=args.low_memory)
+        print("Training VAT...")
         model.fit(trainX, Us=unlabeledX, Y=trainY, update_hook=hooks)
-    elif args.algo == "Pseudo":
+    elif algo == "pseudo":
         model = PseudoLabeler(base_model=base_model,
                               crf_sequence_labeling=args.crf,
                               n_epochs=args.epochs,
                               tensorboard_folder="tensorboard/pseudo",
                               val_interval=2,
-                              val_size=0)
+                              val_size=0,
+                              low_memory_mode=args.low_memory)
+        print(model.get_variable_names())
+        print("Training Pseudo Labels...")
         model.fit(trainX, Us=unlabeledX, Y=trainY, update_hook=hooks)
     else:
         model = SequenceLabeler(base_model=base_model,
                                 crf_sequence_labeling=args.crf,
-                                n_epochs=args.epochs)
+                                n_epochs=args.epochs,
+                                early_stopping_steps=None,
+                                low_memory_mode=args.low_memory)
+        print("Training baseline...")
         model.fit(trainX, trainY, update_hook=hooks)
 
     predictions = model.predict(testX)
