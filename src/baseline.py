bbc67df90524a02c7b55681d1279ba175b444ee1
Eamon Ito-Fisher <eamon@itofisher.com>
2020-07-10 18:21:24 -0700

    FIX, ADD: Bugs with MLM and RoBERTa training fixed, also added automatic handling for data usage and epoch normalization between ssl and baseline experiments

--- src/baseline.py
+++ src/baseline.py
@@ -74,10 +74,10 @@ if __name__ == "__main__":
 
     parser.add_argument('--data_usage',
                      type=int,
-                     default=100,
+                     default=None,
                      help="""What percent of the labeled data that will be
-                     used as labeled data, should be an int 1 - 100.
-                     (default: 100)""")
+                     used as labeled data, should be an int 1 - 100. 
+                     (default: 200 labeled examples)""")
     parser.add_argument('--epochs',
                      type=int,
                      default=2,
@@ -86,6 +86,9 @@ if __name__ == "__main__":
                      type=int,
                      default=1,
                      help="Runs to average over(default: 1)")
+    parser.add_argument("--batch_ratio",
+                     type=float,
+                     default=None)
 
     parser.add_argument('--batch_size',
                      type=int,
@@ -125,6 +128,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+
+
+    algo = None if not args.algo else args.algo.lower()
     config = dict(
         crf_sequence_labeling = args.crf,
         n_epochs = args.epochs,
@@ -143,18 +149,6 @@ if __name__ == "__main__":
         class_weights = args.class_weights
     )
 
-    if args.wandb:
-        import wandb
-        from wandb.tensorflow import WandbHook
-        wandb.init(project="ssl-benchmarks",
-                   name=args.wandb_name,
-                   sync_tensorboard=True,
-                   config=config)
-        wandb.config.dataset = args.data.split('/')[-1]
-        hooks = WandbHook
-    else:
-        hooks = None
-
     filename = args.data
     with open(filename, mode="rb") as f:
         dataset = pickle.load(f)
@@ -167,12 +161,18 @@ if __name__ == "__main__":
         test_size=0.2,
         random_state=42
     )
-    split = 1 - args.data_usage / 100
+
+    config["data_usage"] = args.data_usage
+    if config["data_usage"] is None:
+        default_labeled = 200
+        config["data_usage"] = (default_labeled / len(trainX)) * 100
+
+    split = 1 - config["data_usage"] / 100
     if split > 0.0:
         trainX, unlabeledX, trainY, _ = train_test_split(
             trainX,
             trainY,
-            test_size= 1 - args.data_usage / 100,
+            test_size=split,
             random_state=42
         )
     else:
@@ -185,6 +185,32 @@ if __name__ == "__main__":
     l = len(unlabeledX)
     p = (l / al) * 100
     print(f"{l} examples unlabeled of {al} available - {p:.2f}% of the data")
+    
+    # Adjust epochs to the amount of epochs an SSL approach would see
+    if algo is None or algo == "roberta":
+        if args.batch_ratio:
+            # How many suprevised epochs there are in an unsuprevised epoch
+            # assuming 1:1 batch ratio
+            u_s_len_ratio = len(unlabeledX) / len(trainX)
+            # How many suprevised epochs there are in an unsuprevised epoch,
+            # accounting for different batch_ratios
+            epoch_multiplier = u_s_len_ratio * args.batch_ratio
+            config["n_epochs"] = int(config["n_epochs"] * epoch_multiplier)
+
+    if args.wandb:
+        import wandb
+        from wandb.tensorflow import WandbHook
+        wandb.init(project="ssl-benchmarks",
+                   name=args.wandb_name,
+                   sync_tensorboard=True,
+                   config=config)
+        wandb.config.dataset = args.data.split('/')[-1]
+        wandb.config.algo = algo
+        wandb.config.batch_ratio = args.batch_ratio
+        hooks = WandbHook
+    else:
+        hooks = None
+
 
     arg_base_model = None if not args.base_model else args.base_model.lower()
     if arg_base_model == "tcn":
@@ -200,7 +226,6 @@ if __name__ == "__main__":
         "mean": MeanTeacherLabeler,
         "ict": ICTLabeler
     }
-    algo = None if not args.algo else args.algo.lower()
     if algo in algo_2_model:
         model = algo_2_model[algo](base_model=base_model, **config)
         model.fit(trainX,
@@ -210,17 +235,21 @@ if __name__ == "__main__":
     elif algo is None or algo is "roberta":
         print("Training baseline...")
         model = SequenceLabeler(base_model=base_model,
-                                crf_sequence_labeling=args.crf,
-                                n_epochs=args.epochs,
+                                crf_sequence_labeling=config["crf_sequence_labeling"],
+                                n_epochs=config["n_epochs"],
+                                batch_size=config["batch_size"],
+                                class_weights=config["class_weights"],
                                 early_stopping_steps=None,
-                                low_memory_mode=args.low_memory)
+                                low_memory_mode=config["low_memory_mode"])
         model.fit(trainX, trainY, update_hook=hooks)
     elif algo == "mlm":
         print("Training Masked Language Model...")
         class_weights = config["class_weights"]
         config["class_weights"] = None
+        config["batch_size"] = 48
         model = MaskedLanguageModel(base_model=base_model, **config)
-        model.fit(unlabeledX)
+        mlmX = unlabeledX + trainX
+        model.fit(mlmX)
         save_file = "bert/ssl_mlm.jl" 
         save_file = save_file + str(int(time.time())) 
         model.create_base_model(save_file)
@@ -228,10 +257,9 @@ if __name__ == "__main__":
         model = SequenceLabeler(base_model=base_model,
                                 base_model_path=save_file,
                                 crf_sequence_labeling=args.crf,
-                                n_epochs=10,
+                                n_epochs=20,
                                 early_stopping_steps=None,
                                 low_memory_mode=args.low_memory,
-                                batch_size=args.batch_size,
                                 class_weights=class_weights)
         model.fit(trainX, trainY, update_hook=hooks)
 
