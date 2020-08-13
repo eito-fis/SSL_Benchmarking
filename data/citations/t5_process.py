5c9813424a76df71a22096dde63b55e25f687a29
Eamon Ito-Fisher <eamon@itofisher.com>
2020-08-13 16:21:12 -0700

    ADD: Removed equals and custom, and started newline

--- data/citations/t5_process.py
+++ data/citations/t5_process.py
@@ -14,52 +14,6 @@ def check_level(splits, i):
         j += 1
     return high_level
 
-def process_equals(line):
-    entities = []
-    entity = ""
-    new_tag = True
-    closed = False
-    stack = []
-    sentence = []
-    filter_fn = lambda x: True if x and x != " " else False
-    splits = list(filter(filter_fn, re.split("(<[^>]*>)", line)))[:-1]
-    for i in range(len(splits)):
-        token = splits[i]
-        if len(token) > 2 and token[0] == "<":
-            # Tag found
-            if token[1] == "/":
-                # Closing tag
-                closed = True
-                tag = token[2:-1]
-                assert(len(stack) > 0), f"No tags on stack when {token} was found in {splits}"
-                assert(tag == stack[-1]), f"{token} does not match {stack[-1]} in {splits}"
-                stack.pop()
-                if len(stack) == 0:
-                    assert not new_tag, f"{entity} ended with a tag!"
-                    if len(entity):
-                        entities.append(entity)
-                        entity = ""
-            else:
-                # Opening tag
-                closed = False
-                new_tag = True
-                tag = token[1:-1]
-                stack.append(tag)
-                entity = entity + ("; " if len(entity) else "") + tag
-                high_level = check_level(splits, i)
-        elif len(token) > 0:
-            # Word found
-            token = re.sub(" ([.?!:;,])", "\\1", token)
-            sentence.append(token)
-            if len(entity) == 0 or len(stack) == 0 or closed or high_level:
-                continue
-            if new_tag:
-                entity = entity + " =" + token
-                new_tag = False
-            else:
-                entity += token
-    return "| ".join(entities), " ".join(sentence)
-
 def process_json(line):
     closed = False
     entities = []
@@ -100,48 +54,6 @@ def process_json(line):
                 continue
             entity += token
     return "| ".join(entities), " ".join(sentence)
-
-def process_custom(line):
-    entities = []
-    entity = ""
-    stack = []
-    sentence = []
-    filter_fn = lambda x: True if x and x != " " else False
-    splits = list(filter(filter_fn, re.split("(<[^>]*>)", line)))[:-1]
-    closed = False
-    new_open = True
-    bottom_level = True
-    for i in range(len(splits)):
-        token = splits[i]
-        if len(token) > 2 and token[0] == "<":
-            # Tag found
-            if token[1] == "/":
-                # Closing tag
-                tag = token[2:-1]
-                assert(len(stack) > 0), f"No tags on stack when {token} was found in {splits}"
-                assert(tag == stack[-1]), f"{token} does not match {stack[-1]} in {splits}"
-                stack.pop()
-                closed = True
-                if len(stack) == 0:
-                    if len(entity):
-                        entities.append(entity)
-                        entity = ""
-            else:
-                # Opening tag
-                tag = token[1:-1]
-                stack.append(tag)
-                entity = entity + (" <entity_sep> " if len(entity) else "") + tag
-                closed = False
-                new_open = True
-                high_level = check_level(splits, i)
-        elif len(token) > 0:
-            token = re.sub(" ([.?!:;,])", "\\1", token).strip()
-            sentence.append(token)
-            if len(entity) == 0 or len(stack) == 0 or closed or high_level:
-                continue
-            entity += (" <entity_text> " if new_open else "") + token
-            new_open = False
-    return " | ".join(entities), " ".join(sentence)
 
 def process_original(line):
     entities = []
@@ -185,6 +97,48 @@ def process_original(line):
             entity += " " + token
             new_open = False
     return " |".join(entities).strip(), " ".join(sentence).strip()
+
+def process_newline(line):
+    # { = \n, } = \t
+    closed = False
+    entities = []
+    entity = ""
+    stack = []
+    sentence = []
+    filter_fn = lambda x: True if x and x != " " else False
+    splits = list(filter(filter_fn, re.split("(<[^>]*>)", line)))[:-1]
+    for i in range(len(splits)):
+        token = splits[i]
+        if len(token) > 2 and token[0] == "<":
+            # Tag found
+            if token[1] == "/":
+                # Closing tag
+                closed = True
+                tag = token[2:-1]
+                assert(len(stack) > 0), f"No tags on stack when {token} was found in {splits}"
+                assert(tag == stack[-1]), f"{token} does not match {stack[-1]} in {splits}"
+                stack.pop()
+                if len(stack) == 0:
+                    if len(entity):
+                        entities.append(entity)
+                        entity = ""
+            else:
+                # Opening tag
+                closed = False
+                tag = token[1:-1]
+                stack.append(tag)
+                tabs = "}" * len(stack)
+                entity = entity + ("{" if len(entity) else "") + tag
+                high_level = check_level(splits, i)
+        elif len(token) > 0:
+            # Word found
+            # Label with all tags active in stack
+            token = re.sub(" ([.?!:;,])", "\\1", token)
+            sentence.append(token)
+            if len(entity) == 0 or len(stack) == 0 or closed or high_level:
+                continue
+            entity += token
+    return "| ".join(entities), " ".join(sentence)
 
 def read_file(filename, mode="equals", debug=False):
     with open(filename, mode="rt") as f:
